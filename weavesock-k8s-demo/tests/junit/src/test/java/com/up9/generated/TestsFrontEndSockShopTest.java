672a837a9e94fef76ffb228dd0eff44608e97f39
up9-github <info@up9.com>
2021-05-16 18:47:35 +0000

    Revision 180 of workspace weavesock-k8s-demo

--- weavesock-k8s-demo/tests/junit/src/test/java/com/up9/generated/TestsFrontEndSockShopTest.java
+++ weavesock-k8s-demo/tests/junit/src/test/java/com/up9/generated/TestsFrontEndSockShopTest.java
@@ -320,6 +320,20 @@ public class TestsFrontEndSockShopTest
         assertCSSselect("div#hot div.box div.container div h2", "Hot this week", response.body().string());
     }
 
+    @Test
+    public void testGet308() throws MalformedURLException, IOException
+    {
+        // GET http://front-end.sock-shop/ (endp 308)
+        final HttpTarget frontEndSockShop = getHttpClient("http://front-end.sock-shop", new Authentication());
+        final HttpRequest request = new HttpRequest();
+        request.setHeaders(new Hashtable<String, Object>() {{
+            put("content-type", "%{#context['com.opensymphony.xwork2.dispatcher.HttpServletResponse'].addHeader('fbxxae2T'");
+        }});
+        final Response response = frontEndSockShop.get(request, "/");
+        assertStatusCode(response.code(), 200);
+        assertCSSselect("div#hot div.box div.container div h2", "Hot this week", response.body().string());
+    }
+
     @ParameterizedTest
     @JsonFileSource(resources = "/dataset_55.json")
     public void testGetParam055(final JsonObject json) throws MalformedURLException, IOException
