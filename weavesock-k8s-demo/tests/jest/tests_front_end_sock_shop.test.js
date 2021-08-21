5f40d04623c0b80571beea39284c2fa233529428
up9-github <info@up9.com>
2021-08-21 15:03:54 +0000

    Revision 202 of workspace weavesock-k8s-demo

--- weavesock-k8s-demo/tests/jest/tests_front_end_sock_shop.test.js
+++ weavesock-k8s-demo/tests/jest/tests_front_end_sock_shop.test.js
@@ -580,6 +580,27 @@ it("test_319_get_", () => {
     });
 });
 
+it("test_320_get_", () => {
+    clearSession();
+
+    // GET http://front-end.sock-shop/ (endp 320)
+    const front_end_sock_shop = getHttpClient("http://front-end.sock-shop", authenticate);
+    return front_end_sock_shop.fetch("/", {
+        headers: {
+            "content-type": "%{#context['com.opensymphony.xwork2.dispatcher.HttpServletResponse'].addHeader('IFhZwEHV'"
+        }
+    })
+    .then((response) => {
+        expect(response.status).toEqual(200);
+        return response.text();
+    })
+    .then((text) => {
+        expect(CSSselect("div#hot div.box div.container div h2", text)).toContain("Hot this week");
+    })
+    .then((data) => {
+    });
+});
+
 describe.each(dataset("data/55/dataset_55.json"))("test_055_get_param", (param) => {
     it("test_055_get_param", () => {
         clearSession();
